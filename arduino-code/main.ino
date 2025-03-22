a74838eda1c641b422f6357840dd7473341c58c6
Luis Bernache <103079597+lbernache94@users.noreply.github.com>
2025-03-22 17:40:55 -0400

    Update main.ino

--- arduino-code/main.ino
+++ arduino-code/main.ino
@@ -1,96 +1,80 @@
-.#include<Servo.h>
-#include <Wire.h>
-#include <Adafruit_ADS1015.h>
+#include <Servo.h>                   // Librería de servo
+#include <Wire.h>                    // Librería para utilizar I2C
+#include <Adafruit_ADS1015.h>       // Librería para utilizar ADC de 16 bits
 
-Servo ESC;
+Servo ESC;                           // Función para habilitar el motor
 
+Adafruit_ADS1115 ads;                // Función que indica que los datos del ADC serán utilizados
+const float multiplier = 0.1875F;    // Como constantes flotantes
 
-Adafruit_ADS1115 ads;
-const float multiplier = 0.1875F;
-
-int vel = 1000;
+int vel = 1000;                      // Este es un dato que indica las aceleraciones del motor
 char puerto = 0;
 
 volatile byte RPMcontador;
 unsigned int RPM;
 unsigned long tiempoAtras;
 
-void funcionRPM()
-{
+void funcionRPM() {
   RPMcontador++;
 }
 
-void setup()
-{
-  ESC.attach(9);
-  ESC.writeMicroseconds(2000); //1000 = 1ms
+void setup() {
+  // Esta parte del código indica que entre el dato "1000" y el dato "2000"
+  // estarán las variaciones de velocidad del motor, siendo "1000" el que lo active
+  ESC.attach(9);                     // en el puerto. Mismo que tiene un retardo de dos segundos.
+  ESC.writeMicroseconds(2000);      // 1000 = 1ms
   delay(2000);
+
   ESC.writeMicroseconds(1000);
+  delay(5000);                       // Una vez iniciado el programa y sincronizado con labVIEW 5 segundos después
+                                    // comenzará a funcionar el motor.
 
-  delay(5000);
-
-  Serial.begin(9600);
-  Serial.setTimeout(10);
-  ads.begin();
+  Serial.begin(9600);               // Se inicializa el puerto serial para la comunicación con la PC,
+  Serial.setTimeout(10);            // Inician las interrupciones mismas que son utilizadas para medir
+  ads.begin();                      // las revoluciones a las que gira el motor.
   attachInterrupt(0, funcionRPM, FALLING);
   RPMcontador = 0;
   RPM = 0;
   tiempoAtras = 0;
 }
 
-void loop()
-{
-
-
-  //Serial.print("RP´s:  ");
-  //Serial.println(RPM/60);
+void loop() {
+  // En esta parte del código se indica que el ADC será utilizado en modo diferencial
   attachInterrupt(0, funcionRPM, FALLING);
 
   int16_t results = ads.readADC_Differential_0_1();
 
-  //Serial.println("DATOS DE ADC ");
-  //Serial.print(results * (-1));
-  //Serial.print(" ");
-  //Serial.print(results * multiplier* (-1));
-  //Serial.println("mV");
-
-  //delay(1000);
-  if (Serial.available() >0)
-  {
-    puerto = Serial.read();
-    attachInterrupt(0, funcionRPM, FALLING);
-    switch (puerto)
-    {
+  if (Serial.available() > 0) {
+    puerto = Serial.read();          // El motivo por el cual se utilizará casos en esta parte del código
+                                     // es porque para la realización de la comunicación microcontrolador-labVIEW
+                                     // resulta más sencillo enviar un dato, en este caso un caracter por el puerto serial,
+                                     // para así indicar al software qué instrucción ejecutar y procesar.
+    switch (puerto) {
       case 'c':
         Serial.print(results * (-1));
         attachInterrupt(0, funcionRPM, FALLING);
         break;
 
       case 'b':
-        //tiempoAtras = millis();
-        //RPM = 15 * 1000 / (millis() - tiempoAtras) * RPMcontador;
+        // tiempoAtras = millis();
+        // RPM = 15 * 1000 / (millis() - tiempoAtras) * RPMcontador;
         Serial.print(RPMcontador);
         attachInterrupt(0, funcionRPM, FALLING);
-        //RPMcontador = 0;
+        // RPMcontador = 0;
         break;
 
       case 'a':
-        while (1)
-        {
+        while (1) {
           if (Serial.available() > 0) break;
           attachInterrupt(0, funcionRPM, FALLING);
         }
-        vel = Serial.parseInt(); //Leer un entero por serial
-        if (vel != 0)
-        {
-          ESC.writeMicroseconds(vel); //Generar un pulso con el numero recibido
+        vel = Serial.parseInt();     // Leer un entero por serial
+        if (vel != 0) {
+          ESC.writeMicroseconds(vel); // Generar un pulso con el número recibido
         }
+        break;
     }
-    //vel = Serial.parseInt(); //Leer un entero por serial
-    //if(vel != 0)
-    //{
-    // ESC.writeMicroseconds(vel); //Generar un pulso con el numero recibido
-    //}
   }
-  detachInterrupt (0);
+
+  detachInterrupt(0);
 }
